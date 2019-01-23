bf8632455b602c71d969898a7444e3953876fbfe
leno_zhang <zhangjialuo@126.com>
2019-01-23 15:56:47 +0800

    新建项目

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import MyStudyDemo from '@/pages/myStudy'
+import ClassObject from '@/pages/classObject'
+import styleDemo from '@/pages/styleDemo'
+import EventProcessor from '@/pages/eventProcessor'
+import FormDemo from '@/pages/formDemo'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/myStudy',
+      name: 'myStudyDemo',
+      component: MyStudyDemo
+    },
+    {
+      path:'/classObject',
+      name:'classObject',
+      component:ClassObject
+    },
+    {
+      path:'/styleDemo',
+      name:'styleDemo',
+      component:styleDemo
+    },
+    {
+      path:'/eventProcessor',
+      name:'eventProcessor',
+      component:EventProcessor
+    },
+    {
+      path:'/formDemo',
+      name:'formDemo',
+      component:FormDemo
+    }
+  ]
+})
