a8b0da68da7d6955c212f97471170f933c230a40
leno_zhang <zhangjialuo@126.com>
2019-01-24 17:26:29 +0800

    组件自定义事件。（父组件是使用 props 传递数据给子组件，但如果子组件要把数据传递回去，就需要使用自定义事件！）

--- src/pages/componentDemo.vue
+++ src/pages/componentDemo.vue
@@ -8,18 +8,26 @@
       <ol>
         <todo-item v-for="item in sites" :todo="item"></todo-item>
       </ol>
+
+      <div>
+        <p>{{total}}</p>
+        <button-counter v-on:increment="incrementTotal"></button-counter>
+        <button-counter v-on:increment="incrementTotal"></button-counter>
+      </div>
     </div>
 </template>
 
 <script>
    import customComponent from '../components/customComponent'
    import todoItem from '../components/todoItem'
+   import customEvent from '../components/customEvent'
 
     export default {
         name: "component-demo",
         components:{
           'v-customComponent':customComponent,
-          'todo-item':todoItem
+          'todo-item':todoItem,
+          'button-counter':customEvent,
         },
        data(){
           return{
@@ -28,9 +36,15 @@
               {text:'Runoob'},
               {text:'Google'},
               {text:'Taobao'}
-            ]
+            ],
+            total:0,
           }
-       }
+       },
+      methods:{
+        incrementTotal:function () {
+          this.total += 1;
+        }
+      }
     }
 </script>
 
